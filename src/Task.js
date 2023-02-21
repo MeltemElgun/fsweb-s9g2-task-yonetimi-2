83bb91b0f44dcf2ac87773d6f6cdadcf9c88c25b
MeltemElgun <113603458+MeltemElgun@users.noreply.github.com>
2023-02-22 02:35:50 +0300

    s9g2-task-yonetimi-2-tailwindcss-düzeltilecek

--- src/Task.js
+++ src/Task.js
@@ -1,18 +1,45 @@
 import React from "react";
-
+import { differenceInDays, formatDistanceToNow } from "date-fns";
+import { tr } from "date-fns/locale";
 const Task = ({ taskObj, onComplete }) => {
+  const backgroundColor =
+    differenceInDays(new Date(taskObj.deadline), new Date()) <= 3
+      ? true
+      : false;
 
   return (
-    <div className="task">
-      <h3>{taskObj.title}</h3>
-      <div className="deadline">son teslim: <span>{taskObj.deadline}</span></div>
-      <p>{taskObj.description}</p>
+    <div className="p-6 bg-[#fff] rounded-[5px] leading-normal mt-4 shadow-[0_4px_5px_0_rgba(0,0,0,10%)]">
+      <h3 className="text-[18px] text-[#c8781a]">{taskObj.title}</h3>
+      <div className="text-[12px] pt-[4px]">
+        son teslim:{" "}
+        <span className={backgroundColor ? "bg-[#ffd9d4]" : "bg-[#96635d]"}>
+          {formatDistanceToNow(new Date(taskObj.deadline), {
+            locale: tr,
+            addSuffix: true,
+          })}
+        </span>
+      </div>
+      <p className="p-[0.5rem,0,0.75rem] text-[14px] text-[#444]">
+        {taskObj.description}
+      </p>
       <div>
         {taskObj.people.map((p) => (
-          <span className="pill" key={p}>{p}</span>
+          <span
+            className="inline-block p-[5px,12px] text-[14px] border-[1px] border-solid border-[#ccc] mr-1 mb-[6px] rounded-[30px] "
+            key={p}
+          >
+            {p}
+          </span>
         ))}
       </div>
-      {onComplete && <button onClick={() => onComplete(taskObj.id)}>Tamamlandı</button>}
+      {onComplete && (
+        <button
+          className="block p-[8px,12px] ml-auto bg-[#fecc91] shadow-[0_4px_5px_0_rgba(0,0,0/5%)]  rounded-[3px] border-0 cursor-pointer"
+          onClick={() => onComplete(taskObj.id)}
+        >
+          Tamamlandı
+        </button>
+      )}
     </div>
   );
 };
